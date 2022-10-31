1bf0ec2f1b65d28e76ce6c3a2ebb51ee6c2e7ea7
s148719 <jan.kjetil.skar@nav.no>
2022-10-31 12:56:19 +0100

    Oppsett

---
+++ bidrag-reiesekostnader-api/src/main/java/no/nav/bidrag/reisekostnader/bidragreiesekostnaderapi/BidragReiesekostnaderApiApplication.java
@@ -0,0 +1,13 @@
+package no.nav.bidrag.reisekostnader.bidragreiesekostnaderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidragReiesekostnaderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidragReiesekostnaderApiApplication.class, args);
+	}
+
+}
