adedc1d51f17f9576fe31f5ac0d1320f2d3b792e
s148719 <jan.kjetil.skar@nav.no>
2022-11-23 12:31:14 +0100

    Update BidragReiesekostnadApiApplikasjon.java

--- src/main/java/no/nav/bidrag/reisekostnad/BidragReiesekostnadApiApplikasjon.java
+++ src/main/java/no/nav/bidrag/reisekostnad/BidragReiesekostnadApiApplikasjon.java
@@ -1,12 +1,14 @@
 package no.nav.bidrag.reisekostnad;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client;
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @Slf4j
+@EnableOAuth2Client(cacheEnabled = true)
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJwtTokenValidation(ignore = {"org.springdoc", "org.springframework"})
 public class BidragReiesekostnadApiApplikasjon {
