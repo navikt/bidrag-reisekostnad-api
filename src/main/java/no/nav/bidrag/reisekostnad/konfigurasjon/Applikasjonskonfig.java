e67f8eb2bdf3548a78a9e252f9565873fe350e16
Ugur Cenar <cenar93@gmail.com>
2022-11-25 06:47:28 +0100

    Upgrade bidrag-commons and refactor

--- src/main/java/no/nav/bidrag/reisekostnad/konfigurasjon/Applikasjonskonfig.java
+++ src/main/java/no/nav/bidrag/reisekostnad/konfigurasjon/Applikasjonskonfig.java
@@ -10,8 +10,6 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.bidrag.commons.security.api.EnableSecurityConfiguration;
-import no.nav.bidrag.commons.security.service.OidcTokenManager;
-import no.nav.bidrag.commons.security.utils.TokenUtils;
 import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,7 +35,7 @@ import org.springframework.context.annotation.Profile;
 @EnableSecurityConfiguration
 public class Applikasjonskonfig {
 
-  public static final String ISSUER_TOKENX = "idporten";
+  public static final String ISSUER_TOKENX = "tokenx";
   public static final Logger SIKKER_LOGG = LoggerFactory.getLogger("secureLogger");
 
   @Bean
@@ -46,33 +44,6 @@ public class Applikasjonskonfig {
         .components(new Components()
             .addSecuritySchemes("bearer-key", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"))
         ).info(new io.swagger.v3.oas.models.info.Info().title("bidrag-reisekostnad-api").version("v1"));
-  }
-
-  @Bean
-  public OidcTokenSubjectExtractor oidcTokenSubjectExtractor(OidcTokenManager oidcTokenManager) {
-    return () -> henteIdentFraToken(oidcTokenManager);
-  }
-
-  private String henteIdentFraToken(OidcTokenManager oidcTokenManager) {
-    var ident = TokenUtils.fetchSubject(oidcTokenManager.fetchTokenAsString());
-    return erNumerisk(ident) ? ident : TokenUtils.fetchSubject(oidcTokenManager.fetchTokenAsString());
-  }
-
-  @FunctionalInterface
-  public interface OidcTokenSubjectExtractor {
-
-    String hentPaaloggetPerson();
-  }
-
-  public static boolean erNumerisk(String ident) {
-    try {
-      Long.parseLong(ident);
-      log.info("Identen er numerisk");
-      return true;
-    } catch (NumberFormatException e) {
-      log.warn("Identen er ikke numerisk");
-      return false;
-    }
   }
 
   @Configuration
--- src/main/java/no/nav/bidrag/reisekostnad/konfigurasjon/Tokeninfo.kt
+++ src/main/java/no/nav/bidrag/reisekostnad/konfigurasjon/Tokeninfo.kt
@@ -5,23 +5,23 @@ import no.nav.bidrag.commons.security.utils.TokenUtils
 import no.nav.bidrag.commons.security.utils.TokenUtils.fetchSubject
 import org.springframework.stereotype.Service
 
-@Service
-class Tokeninfo(
-    private val oidcTokenManager: OidcTokenManager
-) {
-    fun erSystembruker(): Boolean {
-        return try {
-            TokenUtils.isSystemUser(oidcTokenManager.fetchTokenAsString())
-        } catch (e: Exception) {
-            false
+class Tokeninfo {
+    companion object {
+        private val oidcTokenManager: OidcTokenManager = OidcTokenManager()
+        fun erSystembruker(): Boolean {
+            return try {
+                TokenUtils.isSystemUser(oidcTokenManager.fetchTokenAsString())
+            } catch (e: Exception) {
+                false
+            }
+        }
+        fun hentPaaloggetPerson(): String? {
+            return try {
+                fetchSubject(oidcTokenManager.fetchTokenAsString())
+            } catch (e: Exception) {
+                null
+            }
         }
     }
 
-    fun hentPaaloggetPerson(): String? {
-        return try {
-            fetchSubject(oidcTokenManager.fetchTokenAsString())
-        } catch (e: Exception) {
-            null
-        }
-    }
 }
