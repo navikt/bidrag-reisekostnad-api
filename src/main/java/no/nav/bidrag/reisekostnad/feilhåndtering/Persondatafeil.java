515a5eff62f1d3f4851dfff7cfe47f47d251edcd
Ugur Cenar <cenar93@gmail.com>
2022-12-09 16:32:29 +0100

    Rollback unecessary changes

--- src/main/java/no/nav/bidrag/reisekostnad/feilhåndtering/Persondatafeil.java
+++ src/main/java/no/nav/bidrag/reisekostnad/feilhåndtering/Persondatafeil.java
@@ -1,10 +1,8 @@
 package no.nav.bidrag.reisekostnad.feilhåndtering;
 
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 
-@Slf4j
 @Getter
 public class Persondatafeil extends ReisekostnadApiFeil {
 
--- src/main/java/no/nav/bidrag/reisekostnad/feilhåndtering/Valideringsfeil.java
+++ src/main/java/no/nav/bidrag/reisekostnad/feilhåndtering/Valideringsfeil.java
@@ -1,10 +1,8 @@
 package no.nav.bidrag.reisekostnad.feilhåndtering;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Slf4j
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class Valideringsfeil extends ReisekostnadApiFeil {
 
