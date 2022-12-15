230d09c3634e8bee0335beae2e2b8cbb24b0cbaf
s148719 <jan.kjetil.skar@nav.no>
2022-12-15 17:20:19 +0100

    Update ReisekostnadApiTjenesteTest.java

--- src/test/java/no/nav/bidrag/reisekostnad/tjeneste/støtte/ReisekostnadApiTjenesteTest.java
+++ src/test/java/no/nav/bidrag/reisekostnad/tjeneste/støtte/ReisekostnadApiTjenesteTest.java
@@ -39,7 +39,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.annotation.DirtiesContext;
 
+@DirtiesContext
 @ExtendWith(MockitoExtension.class)
 public class ReisekostnadApiTjenesteTest {
 
