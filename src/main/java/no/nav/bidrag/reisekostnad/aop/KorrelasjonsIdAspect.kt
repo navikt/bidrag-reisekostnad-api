d57af5d4a62052b97d38de458415182157b5d0cd
Ugur Cenar <cenar93@gmail.com>
2022-12-15 11:59:50 +0100

    Fiks ugyldig korrelasjonsid

--- src/main/java/no/nav/bidrag/reisekostnad/aop/KorrelasjonsIdAspect.kt
+++ src/main/java/no/nav/bidrag/reisekostnad/aop/KorrelasjonsIdAspect.kt
@@ -1,22 +1,25 @@
 package no.nav.bidrag.reisekostnad.aop
 
-import no.nav.bidrag.commons.CorrelationId
-import no.nav.bidrag.reisekostnad.model.KORRELINGSID
+import no.nav.bidrag.reisekostnad.model.MDC_KORRELASJONSID
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.After
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.slf4j.MDC
 import org.springframework.stereotype.Component
+import java.net.URLEncoder
+import java.util.UUID
 
 @Component
 @Aspect
-class CorrelationIdAspect {
+class KorrelasjonsIdAspect {
 
     @Before(value = "execution(* no.nav.bidrag.reisekostnad.skedulering.Databehandler.*(..))")
     fun schedulerCorrelationIdToThread(joinPoint: JoinPoint) {
-        val correlationId = CorrelationId.generateTimestamped(joinPoint.signature.name).get()
-        MDC.put(KORRELINGSID, correlationId)
+        val tilfeldigVerdi = UUID.randomUUID().toString().subSequence(0, 8)
+        val enkodedMetodenavn = URLEncoder.encode(joinPoint.signature.name, "utf-8")
+        val korrelasjonsId = "${tilfeldigVerdi}_$enkodedMetodenavn"
+        MDC.put(MDC_KORRELASJONSID, korrelasjonsId)
     }
 
     @After(value = "execution(* no.nav.bidrag.reisekostnad.skedulering.Databehandler.*(..))")
--- src/main/java/no/nav/bidrag/reisekostnad/model/Konstanter.kt
+++ src/main/java/no/nav/bidrag/reisekostnad/model/Konstanter.kt
@@ -3,6 +3,6 @@ package no.nav.bidrag.reisekostnad.model
 import java.time.LocalDate
 
 const val SYSTEMBRUKER_ID = "SYSTEM"
-const val KORRELINGSID = "correlationId"
+const val MDC_KORRELASJONSID = "correlationId"
 
 val dato15ÅrTilbakeFraIdag = LocalDate.now().minusYears(15)
